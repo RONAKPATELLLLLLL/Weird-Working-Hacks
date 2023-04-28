795a9aed398745a61a0b40661c83aaafffbfe696
RONAKPATELLLLLLL <121185799+RONAKPATELLLLLLL@users.noreply.github.com>
2023-04-28 13:53:32 -0600

    Create Bookmarklet.js

---
+++ Tutorials/Bookmarklet.js
@@ -0,0 +1,20 @@
+
+# How to use the bookmarklets.js
+
+1. Copy the entire thing using the two box looking things in the corner of the bar over the code
+
+2. Click on the boxes and it will become a checkmark
+
+3. Go up to the bookmark star
+
+4. Click on it
+
+5. Go to More...
+
+6. Go to URL
+
+7. Paste it there
+
+8. Press Save
+
+9. You're done!
