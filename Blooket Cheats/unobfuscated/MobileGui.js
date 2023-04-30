541116138ecab15e544fb97869858a80fac2a610
RONAKPATELLLLLLL <121185799+RONAKPATELLLLLLL@users.noreply.github.com>
2023-04-30 16:39:49 -0600

    Update MobileGui.js

--- Blooket Cheats/unobfuscated/MobileGui.js
+++ Blooket Cheats/unobfuscated/MobileGui.js
@@ -1,13 +1,13 @@
 /**
 * @license StewartPrivateLicense-2.0.1
-* Copyright (c) Aerell McKnight 2023
+* Copyright (c) RONAKPATELLLLLLL 2023
 *
 * You may not reproduce or distribute any code inside this file without the licenser's permission.
 * You may not copy, modify, steal, skid, or recreate any of the code inside this file.
 * You may not under any circumstance republish any code from this file as your own.
 * 
 * ALL TERMS STATED IN THE LINK BELOW APPLY ASWELL
-* https://github.com/Minesraft2/Blooket-Cheats/blob/main/LICENSE
+* https://github.com/RONAKPATELLLLLLL/Weird-Working-Hacks/blob/main/License
 */
 
 /* THE UPDATE CHECKER IS ADDED DURING COMMIT PREP, THERE MAY BE REDUNDANT CODE, DO NOT TOUCH */
